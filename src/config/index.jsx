5a273fef01718b38e31e22ecbe7d7cc22d119206
suruchi810 <161755548+suruchi810@users.noreply.github.com>
2025-04-09 13:56:14 +0530

    Update index.jsx

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,6 +1,6 @@
 const {default: axios} = require('axios');
 
-export const BASE_URL  = 'https://linkedinbackend-1-zyst.onrender.com'
+export const BASE_URL  = 'https://linkedinbackend-i0sg.onrender.com'
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
